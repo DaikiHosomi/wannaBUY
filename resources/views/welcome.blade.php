019709b63b5eff78c8619a06fbee30c45abc6cac
DaikiHosomi <daikho2021@gmail.com>
2020-04-29 17:15:50 +0900

    レスポンシブ対応('LP')

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -22,78 +22,87 @@
     <body>
         <header class="masthead">
             <div class="container">
-                <div class="intro-text">
-                    <div class="intro-heading">wannaBUY</div>
-                </div>
- 
-                    @guest
-                    <div class="button">  
-                        <a href="{{ route('login') }}" class="btn-square-so-login mx-5">ログインする</a>            
-                        <a href="{{ route('register') }}" class="btn-square-so-register mx-5">会員登録する</a>
+                <div class="row justify-content-center">
+                    <div class="col-md-12">
+                        <div class="intro-heading text-center">wannaBUY</div>
                     </div>
+
+                    @guest
+                    <div class="button col-md-6">
+                        <a href="{{ route('login') }}" class="btn-square-so-login mx-5">ログインする</a>
+                    </div>
+                    <div class="col-md-6">
+                        <a href="{{ route('register') }}" class="btn-square-so-register mx-5">会員登録する</a>   
+                    </div>               
                     @else
-                    <div class="button">
+
+                    <div class="col-md-12">
                         <a href="{{ route('products.index') }}" class="btn-square-so-login mx-5">ホームへ</a> 
                     </div>
                     @endguest
+                        
+                </div>
+            
+                </div>        
             </div>
         </header>  
 
             <section class="features">
                 <div class="container">
-                    <div class="row">
+                    <div class="row justify-content-center">
                         <div class="col-md-12 feature-top">
                             <div class="feature-title">What is "WannaBUY"?</div>
-                            <div class="feature-heading my-5">『完全APU生間　参考書売買サービス』</div>
-                        <div>
+                            <div class="feature-heading mt-5 mb-3">『完全APU生間　参考書売買サービス』</div>
+                        </div>
 
                         <div class="col-md-12 text-center">
                             <h1 class="feature">特徴</h1>
                         </div>
                     </div>
-                
+
                     <div class="row">
-                        <div class="col-md-6 px-5 my-5" >
+
+                        <div class="col-md-6 text-center">
                             <div class="feature-box box">
-                                <div class="feature-list">
+                                <div class="feature-list p-3">
                                     <i class="fas fa-search pr-3"></i>学部、講義名で検索できる </div>
-                                <p class="feature-text pt-5">学部（APS/APM）、専門分野、言語（英語/日本語）基準などのカテゴリ検索が可能です。</p>
+                                <p class="feature-text pt-2 pb-4 px-5">学部（APS/APM）、専門分野、言語（英語/日本語）基準などのカテゴリ検索が可能です。</p>
                             </div>
                         </div>
 
-                        <div class="col-md-6 px-5 my-5">
+                        <div class="col-md-6 text-center">
                             <div class="feature-box box">
-                                <div class="feature-list">
+                                <div class="feature-list p-3">
                                     <i class="fas fa-university pr-3"></i>APU生同士の売買できる </div>
-                                <p class="feature-text pt-5">APU生同士のみで交渉と取引を行なっていただけます。取引方法は、基本「天空」もしくは「下界」での現金受渡しです。</p>
-                        </div>
-                        </div>
-                    </div>
-
-                        <div class="row row2">
-                            <div class="col-md-6 px-5 my-5">
-                                <div class="feature-box box">
-                                    <div class="feature-list">
-                                        <i class="fas fa-check-square pr-3"></i>販売状況が一目判断できる</div>
-                                    <p class="feature-text pt-5">販売状況から、価格の相場や講義で必要な参考文献を知ることができます。</p>
-                                </div>
-                            </div> 
-
-                            <div class="col-md-6 px-5 my-5">
-                                <div class="feature-box box">
-                                    <div class="feature-list pt-5t">
-                                        <i class="fas fa-chalkboard-teacher pr-3"></i>掲示板で販売を募れる</div>
-                                    <p class="feature-text pt-5"> 必要になった参考書を掲示板で募ることができます。</p>
-                                </div>
+                                <p class="feature-text pt-2 pb-4 px-5">APU生同士のみで交渉と取引を行なっていただけます。取引方法は、基本「天空」もしくは「下界」での現金受渡しです。</p>
                             </div>
                         </div>
+
+                    
+                        <div class="col-md-6 text-center">
+                            <div class="feature-box box">
+                                <div class="feature-list p-3">
+                                    <i class="fas fa-check-square pr-3"></i>販売状況が一目判断できる</div>
+                                <p class="feature-text pt-2 pb-4 px-5">販売状況から、価格の相場や講義で必要な参考文献を知ることができます。</p>
+                            </div>
+                        </div> 
+
+                        <div class="col-md-6 text-center">
+                            <div class="feature-box box">
+                                <div class="feature-list p-3">
+                                    <i class="fas fa-chalkboard-teacher pr-3"></i>掲示板で販売を募れる</div>
+                                <p class="feature-text pt-2 pb-4 px-5"> 必要になった参考書を掲示板で募ることができます。</p>
+                            </div>
+                        </div>
+
                     </div>
-                </div>
+                   
+                </div>      
             </section>
 
             <section class="way-to-use">
                 <div class="container">
-                    <div class="row">
+                    <div class="row justify-content-center">
                         <div class="col-md-12">
                             <div class="way-title mt-5">How to Use</div>
                         </div>
@@ -103,12 +112,12 @@
                         </div>
                     </div>
 
-                    <div class="row my-5">
+                    <div class="row my-5 justify-content-center">
                         <div class="col-md-6">
                             <img src="https://cdn.pixabay.com/photo/2017/08/06/22/12/books-2596900_1280.jpg"  class="d-none d-sm-block image-list img-fluid"  alt="">
                         </div>
                         <div class="col-md-6">
-                            <div class="way-box box mt-5">
+                            <div class="way-box box">
                                 <div class="way-item py-3">In selling</div>
                                 <ol style="list-style-type: decimal"  class="way-text py-1">
                                     <li>APU講義で使用した参考書を準備 </li>
@@ -122,9 +131,9 @@
                         </div>
                     </div>
 
-                    <div class="row my-5">
+                    <div class="row my-5 justify-content-center">
                         <div class="col-md-6">
-                            <div class="way-box box mt-5">
+                            <div class="way-box box">
                                 <div class="way-item py-3">In buying</div>
                                 <ol style="list-style-type: decimal"  class="way-text py-1">
                                     <li>学部名・講義名・講義カテゴリーなどで商品検索</li>
@@ -136,16 +145,16 @@
                             </div>
                         </div>
                         <div class="col-md-6">
-                            <img src="https://cdn.pixabay.com/photo/2018/05/28/11/51/woman-3435842_1280.jpg" class="image-list img-fluid" alt="">                 
+                            <img src="https://cdn.pixabay.com/photo/2018/05/28/11/51/woman-3435842_1280.jpg" class="d-none d-sm-block image-list img-fluid" alt="">                 
                         </div>
                     </div>
 
                     <div class="row my-5">
                         <div class="col-md-6">
-                            <img src="https://www.apu.ac.jp/home/img/contents/campusmap.html/a.jpg" class="image-list img-fluid"  alt="">
+                            <img src="https://www.apu.ac.jp/home/img/contents/campusmap.html/a.jpg" class="d-none d-sm-block image-list img-fluid"  alt="">
                         </div>
                         <div class="col-md-6">
-                            <div class="way-box box mt-5">
+                            <div class="way-box box">
                                 <div class="way-item py-3">In actioning</div>
                                 <ol style="list-style-type: decimal"  class="way-text py-1">
                                     <li>交渉受付ページから交渉画面へ行く</li>
@@ -172,7 +181,7 @@
 
                     <div class="row">
                         <div class="col-md-12">
-                            <div class="last-text">wannaBUYは、APU生向けサービスです。是非APU生で助け合って参考書を手に入れましょう！</div>
+                            <div class="last-text">wannaBUYは、APU生向けサービスです。<br>是非APU生で助け合って参考書を手に入れましょう！</div>
                         </div>
                     </div>
 
